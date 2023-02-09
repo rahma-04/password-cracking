8d90fb98d6e815b5daa5ecc15e0e0a688f3b8382
Rahmalia Rahadi <85733183+rahma-04@users.noreply.github.com>
2023-02-09 21:44:34 +0700

    Add files via upload

---
+++ client_4.py
@@ -0,0 +1,27 @@
+#Client 4 Side
+import xmlrpc.client
+import time
+import numpy as np
+from collections import Counter
+s = xmlrpc.client.ServerProxy('http://127.0.0.1:8000')
+now = time.time()
+
+print("Mengkoneksikan dengan server ..")
+
+text_file = open("kamus.txt", "r")
+lines = text_file.readlines()
+
+for i in range(9000, 1199):
+    ketemu = False
+    s.getWordWorker(lines[i].strip())
+    print(s.bruteforce())
+  
+    if s.bruteforce() == True:
+        ketemu = True 
+        print(f"Selamat kamu telah memecahkan password! Passwordnya adalah: {s.gotcha()}")
+        break
+    
+if ketemu == False:
+    print("Kamu gagal memecahkan password!")
+
+print(f"Waktu eksekusi: {time.time()- now} detik")
